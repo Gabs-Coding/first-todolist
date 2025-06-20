7e4d9021f30343e2eb7613b74134225b13e0a0e3
Gabs-Coding <gsbiel62@gmail.com>
2025-06-20 18:46:19 -0400

    Add definition of the method that fetch for all the tasks from a user by his UUID

--- src/main/java/coding/gabs/todolist/repository/TaskRepository.java
+++ src/main/java/coding/gabs/todolist/repository/TaskRepository.java
@@ -2,7 +2,11 @@ package coding.gabs.todolist.repository;
 
 import coding.gabs.todolist.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+    Optional<List<Task>> findAllByOwnerId(UUID ownerId);
 }
