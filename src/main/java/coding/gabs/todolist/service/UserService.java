eba1b213144e364991bf23fbaba238b96048afcd
Gabs-Coding <gsbiel62@gmail.com>
2025-06-20 13:24:50 -0400

    Add a new validation for the createUser method that validate if the inputted user not already exists in the database to insert it.

--- src/main/java/coding/gabs/todolist/service/UserService.java
+++ src/main/java/coding/gabs/todolist/service/UserService.java
@@ -2,12 +2,14 @@ package coding.gabs.todolist.service;
 
 import coding.gabs.todolist.entity.User;
 import coding.gabs.todolist.exception.InvalidCredentials;
+import coding.gabs.todolist.exception.UserAlreadyExistsInDatabase;
 import coding.gabs.todolist.exception.UserNotFound;
 import coding.gabs.todolist.repository.UserRepository;
 import coding.gabs.todolist.util.PasswordEncryption;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,9 +22,19 @@ public class UserService {
     }
 
     public User createUser(@NotNull User user) {
+        validateIfIsValidNewUser(user);
+
         String hashedPassword = PasswordEncryption.encryptPassword(user.getPassword());
         user.setPassword(hashedPassword);
         return this.userRepository.save(user);
+    }
+
+    private void validateIfIsValidNewUser(User user) {
+        Optional<User> userFromDatabase = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDatabase.isPresent()) {
+            throw new UserAlreadyExistsInDatabase("The user already exists in the database.");
+        }
     }
 
     public User getUserById(@NotNull UUID id) {
@@ -53,5 +65,15 @@ public class UserService {
         }
 
         return user;
+    }
+
+    public List<User> getUsers() {
+        List<User> users = userRepository.findAll();
+
+        if (users.isEmpty()) {
+            throw new UserNotFound("Any user was found in the database.");
+        }
+
+        return users;
     }
 }
