110ac19951ac5c5079132eacbba26f93bf518c7e
Gabs-Coding <gsbiel62@gmail.com>
2025-06-20 12:16:42 -0400

    Add a validation that assert that a task ending date must occur after starting time.

--- src/main/java/coding/gabs/todolist/service/TaskService.java
+++ src/main/java/coding/gabs/todolist/service/TaskService.java
@@ -1,6 +1,7 @@
 package coding.gabs.todolist.service;
 
 import coding.gabs.todolist.entity.Task;
+import coding.gabs.todolist.exception.InvalidLocalDateTime;
 import coding.gabs.todolist.repository.TaskRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,12 @@ public class TaskService {
 
     public Task createTask(Task task, HttpServletRequest request) {
         task.setOwner((UUID) request.getAttribute("userId"));
+
+        if (task.getEndingDateTime() != null &&
+            task.getStartDateTime().isAfter(
+                    task.getEndingDateTime())) {
+            throw new InvalidLocalDateTime("Ending time must be after starting time.");
+        }
 
         return taskRepository.save(task);
     }
