53697d8e6006b7fc01ff52df4229201b3860ead6
Gabs-Coding <gsbiel62@gmail.com>
2025-06-20 18:44:52 -0400

    Put the responsibility to persist the URI from the context of the creation of the entity to the service layer. Add a new method to get all tasks from the user that make the request.

--- src/main/java/coding/gabs/todolist/service/TaskService.java
+++ src/main/java/coding/gabs/todolist/service/TaskService.java
@@ -2,10 +2,14 @@ package coding.gabs.todolist.service;
 
 import coding.gabs.todolist.entity.Task;
 import coding.gabs.todolist.exception.InvalidLocalDateTime;
+import coding.gabs.todolist.exception.TaskNotFound;
 import coding.gabs.todolist.repository.TaskRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -13,12 +17,14 @@ public class TaskService {
 
     private final TaskRepository taskRepository;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, UserService userService) {
         this.taskRepository = taskRepository;
     }
 
     public Task createTask(Task task, HttpServletRequest request) {
-        task.setOwner((UUID) request.getAttribute("userId"));
+        task.setOwnerId((UUID) request.getAttribute("userId"));
+        URI taskURI = URI.create("/task/" + task.getId());
+        request.setAttribute("taskURI", taskURI);
 
         if (task.getEndingDateTime() != null &&
             task.getStartDateTime().isAfter(
@@ -27,5 +33,16 @@ public class TaskService {
         }
 
         return taskRepository.save(task);
+    }
+
+    public List<Task> getTasksFromActiveUser(HttpServletRequest request) {
+        UUID taskOwnerId = (UUID) request.getAttribute("userId");
+        Optional<List<Task>> tasks = this.taskRepository.findAllByOwnerId(taskOwnerId);
+
+        if (tasks.isEmpty()) {
+            throw new TaskNotFound("Any tasks attributed to this owner has not been found.");
+        }
+
+        return tasks.get();
     }
 }
