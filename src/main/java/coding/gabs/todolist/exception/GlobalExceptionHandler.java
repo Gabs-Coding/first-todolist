6baf185da750ac2e1d2e3dca012d944bc4ff1971
Gabs-Coding <gsbiel62@gmail.com>
2025-06-20 13:16:24 -0400

    Implement a more descriptive and complete error message.

--- src/main/java/coding/gabs/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/coding/gabs/todolist/exception/GlobalExceptionHandler.java
@@ -1,29 +1,30 @@
 package coding.gabs.todolist.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsInDatabase.class)
-    public ResponseEntity<String> userAlreadyExistsInDatabaseException(UserAlreadyExistsInDatabase userAlreadyExistsInDatabase) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(userAlreadyExistsInDatabase.getMessage());
+    public ProblemDetail userAlreadyExistsInDatabaseException(UserAlreadyExistsInDatabase userAlreadyExistsInDatabase) {
+        ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, userAlreadyExistsInDatabase.getMessage());
+        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, userAlreadyExistsInDatabase.getMessage());
     }
 
     @ExceptionHandler(InvalidLocalDateTime.class)
-    public ResponseEntity<String> invalidLocalDateTimeException(InvalidLocalDateTime exception) {
-        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(exception.getMessage());
+    public ProblemDetail invalidLocalDateTimeException(InvalidLocalDateTime exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
     }
 
     @ExceptionHandler(InvalidCredentials.class)
-    public ResponseEntity<String> invalidCredentialsException(InvalidCredentials exception) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+    public ProblemDetail invalidCredentialsException(InvalidCredentials exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
     }
 
     @ExceptionHandler(UserNotFound.class)
-    public ResponseEntity<String> userNotFoundException(UserNotFound exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    public ProblemDetail userNotFoundException(UserNotFound exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 }
