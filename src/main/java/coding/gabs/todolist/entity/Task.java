061d9cab8c78aadff99fad264a7000ec764fa7e3
Gabs-Coding <gsbiel62@gmail.com>
2025-06-21 14:43:04 -0400

    Add some validation for the attributes of the Task entity.

--- src/main/java/coding/gabs/todolist/entity/Task.java
+++ src/main/java/coding/gabs/todolist/entity/Task.java
@@ -3,6 +3,7 @@ package coding.gabs.todolist.entity;
 import coding.gabs.todolist.entity.enums.Priority;
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
@@ -13,17 +14,34 @@ import java.util.UUID;
 public class Task {
     @Id
     @GeneratedValue(generator = "UUID")
+    @Column(updatable = false, nullable = false)
     private UUID id;
+    @Column(nullable = false)
     private UUID ownerId;
     @Column(length = 50, nullable = false)
     private String title;
+    @Column(length = 512)
     private String description;
     @CreationTimestamp
     private LocalDateTime creationDateTime;
-    private LocalDateTime startDateTime = LocalDateTime.now();
+    @CreationTimestamp
+    private LocalDateTime startDateTime;
     private LocalDateTime updateDateTime;
     private LocalDateTime endingDateTime;
     private boolean isCompleted = false;
     @Enumerated(EnumType.STRING)
     private Priority priority;
+
+
+    public void setTitle(String title) {
+        if (title == null) {
+            throw new IllegalArgumentException("title cannot be null");
+        }
+
+        if (title.length() > 50) {
+            throw new IllegalArgumentException("title length must be less than 50 characters");
+        }
+
+        this.title = title;
+    }
 }
