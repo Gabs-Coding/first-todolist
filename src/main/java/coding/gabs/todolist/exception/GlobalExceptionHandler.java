d97fa7dca88f1e470c17ff0c203e4c16a7aacb5c
Gabs-Coding <gsbiel62@gmail.com>
2025-06-21 14:37:48 -0400

    Adjust the formatting of the methods and add a new method for handle a HttpMessageNotReadableException.

--- src/main/java/coding/gabs/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/coding/gabs/todolist/exception/GlobalExceptionHandler.java
@@ -2,29 +2,58 @@ package coding.gabs.todolist.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsInDatabase.class)
-    public ProblemDetail userAlreadyExistsInDatabaseException(UserAlreadyExistsInDatabase userAlreadyExistsInDatabase) {
-        ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, userAlreadyExistsInDatabase.getMessage());
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, userAlreadyExistsInDatabase.getMessage());
+    public ProblemDetail userAlreadyExistsInDatabaseException(UserAlreadyExistsInDatabase e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT,
+                e.getMessage());
     }
 
     @ExceptionHandler(InvalidLocalDateTime.class)
-    public ProblemDetail invalidLocalDateTimeException(InvalidLocalDateTime exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
+    public ProblemDetail invalidLocalDateTimeException(InvalidLocalDateTime e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                e.getMessage());
     }
 
     @ExceptionHandler(InvalidCredentials.class)
-    public ProblemDetail invalidCredentialsException(InvalidCredentials exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, exception.getMessage());
+    public ProblemDetail invalidCredentialsException(InvalidCredentials e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNAUTHORIZED,
+                e.getMessage());
     }
 
     @ExceptionHandler(UserNotFound.class)
-    public ProblemDetail userNotFoundException(UserNotFound exception) {
-        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+    public ProblemDetail userNotFoundException(UserNotFound e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND,
+                e.getMessage());
+    }
+
+    @ExceptionHandler(TaskNotFound.class)
+    public ProblemDetail taskNotFoundException(TaskNotFound e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND,
+                e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotAuthorized.class)
+    public ProblemDetail userNotAuthorizedException(UserNotAuthorized e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNAUTHORIZED,
+                e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ProblemDetail httpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNPROCESSABLE_ENTITY,
+                e.getMostSpecificCause().getMessage());
     }
 }
